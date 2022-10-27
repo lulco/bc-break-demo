8906abaacec4559885792812a43b2a41a1aaeff9
Michal Lulco <lulco@efabrica.sk>
2022-10-27 09:36:35 +0200

    Added forms

--- src/Demo/Demo.php
+++ src/Demo/Demo.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace BcBreakDemo\Demo;
 
+use Nette\Forms\Rendering\DefaultFormRenderer;
 use Nette\PhpGenerator\Extractor;
 
-final class Demo
+final class Demo extends DefaultFormRenderer
 {
     public function xxx(): void
     {
